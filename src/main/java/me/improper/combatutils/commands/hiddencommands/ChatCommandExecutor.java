2efd50cc119513a40bdbac54ccb98c3fb1a42586
ImproperIssues <114215797+ItziSpyder@users.noreply.github.com>
2023-02-04 22:46:45 -0800

    Delete src/main directory

--- src/main/java/me/improper/combatutils/commands/hiddencommands/ChatCommandExecutor.java
+++
@@ -1,9 +0,0 @@
-package me.improper.combatutils.commands.hiddencommands;
-
-import org.bukkit.command.CommandSender;
-
-@FunctionalInterface
-public interface ChatCommandExecutor {
-
-    boolean execute(CommandSender sender, ChatCommand command);
-}
