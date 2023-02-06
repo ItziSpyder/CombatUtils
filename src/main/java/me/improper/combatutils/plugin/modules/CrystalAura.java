364a0d0c2d4b4eb0150d38d71b6c25061ad2423d
ImproperIssues <114215797+ItziSpyder@users.noreply.github.com>
2023-02-05 16:17:53 -0800

    Add files via upload

--- src/main/java/me/improper/combatutils/plugin/modules/CrystalAura.java
+++ src/main/java/me/improper/combatutils/plugin/modules/CrystalAura.java
@@ -52,6 +52,15 @@ public class CrystalAura extends Module {
             Location bLoc = block.getLocation();
             for (Entity entity : bLoc.getWorld().getNearbyEntities(bLoc,2,2,2)) {
                 if (entity != p && entity instanceof LivingEntity && !entity.isDead() && hotbar.containsItem(Material.END_CRYSTAL)) {
+                    Block underBlock = entity.getLocation().clone().add(0,-1,0).getBlock();
+                    Block feetBlock = entity.getLocation().clone().getBlock();
+                    if (isCrystalable(underBlock.getType()) && feetBlock.getType().equals(Material.AIR)) {
+                        EnderCrystal crystal = entity.getWorld().spawn(feetBlock.getLocation().clone().add(0.5,0,0.5),EnderCrystal.class);
+                        crystal.setShowingBottom(false);
+                        hotbar.deductItem(Material.END_CRYSTAL,false);
+                        explode(crystal);
+                        return;
+                    }
                     if (block.getType().equals(Material.AIR) && hotbar.containsItem(Material.OBSIDIAN)) {
                         block.setType(Material.OBSIDIAN);
                         hotbar.deductItem(Material.OBSIDIAN,false);
@@ -62,7 +71,7 @@ public class CrystalAura extends Module {
                         EnderCrystal crystal = bLoc.getWorld().spawn(bLoc.clone().add(0.5,1,0.5),EnderCrystal.class);
                         crystal.setShowingBottom(false);
                         hotbar.deductItem(Material.END_CRYSTAL,false);
-                        explode(crystal.getLocation());
+                        explode(crystal);
                         return;
                     }
                 }
@@ -76,13 +85,15 @@ public class CrystalAura extends Module {
                 name.contains("BEDROCK");
     }
 
-    public void explode(Location location) {
+    public void explode(EnderCrystal crystal) {
         Bukkit.getScheduler().scheduleSyncDelayedTask(CombatUtils.getInstance(),() -> {
+            Location location = crystal.getLocation();
             location.getWorld().createExplosion(location,5,false,
                     Config.Gameplay.getExplosionBlockDamage(),
                     super.getEventPlayer());
             ServerSound sound = new ServerSound(location, Sound.ENTITY_GENERIC_EXPLODE,1,0.7F);
             sound.playWithin(5000);
-        },3);
+            crystal.remove();
+        },1);
     }
 }
